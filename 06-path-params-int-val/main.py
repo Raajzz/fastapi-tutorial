8d2c20e1324a46ce142c11a600ba3ae246be1638
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-05-06 19:24:45 +0530

    Completed path params and numeric validations

--- 06-path-params-int-val/main.py
+++ 06-path-params-int-val/main.py
@@ -7,3 +7,37 @@ app = FastAPI()
 async def read_items(item_id : Optional[str] = Path(None, min_length=5, max_length=15)):
   item_result = {"item_id":item_id}
   return item_result
+
+
+@app.get("/arg_order/{item_id}")
+async def arg_order(
+  *,
+  item_id: int = Path(..., title="The ID of the item to get"),
+  q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/page/{item_id}")
+async def page(
+  *,
+  item_id: int = Path(..., title="The ID of the item to get", ge = 0, le = 14 ),
+  q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/gtle/{item_id}")
+async def page(
+  *,
+  item_id: float = Path(..., title="The ID of the item to get", gt = 0, lt = 14 ),
+  q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
